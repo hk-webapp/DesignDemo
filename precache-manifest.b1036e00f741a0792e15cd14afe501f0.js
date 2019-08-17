bde17bb2f9b2101fc76997bf26e8894bb1785b12
hajar <hajar.kian@yahoo.com>
2019-08-17 18:29:31 +0430

    Initial commit

---
+++ precache-manifest.b1036e00f741a0792e15cd14afe501f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43caeee2b166440dc6670d6755548494",
+    "url": "/DesignDemo/index.html"
+  },
+  {
+    "revision": "82449e701466ece6b3d0",
+    "url": "/DesignDemo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0086adb44221b829258c",
+    "url": "/DesignDemo/static/css/main.15354dab.chunk.css"
+  },
+  {
+    "revision": "82449e701466ece6b3d0",
+    "url": "/DesignDemo/static/js/2.62a76ffb.chunk.js"
+  },
+  {
+    "revision": "0086adb44221b829258c",
+    "url": "/DesignDemo/static/js/main.3244ec0b.chunk.js"
+  },
+  {
+    "revision": "2c1b5a789eee4a0d903c",
+    "url": "/DesignDemo/static/js/runtime~main.e1250d82.js"
+  },
+  {
+    "revision": "0092e9206572a7fa11baa0f5d6232efb",
+    "url": "/DesignDemo/static/media/logo.0092e920.svg"
+  }
+]);
